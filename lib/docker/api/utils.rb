2131d447e3474494b8627bc75d52cee46d7feaa0
Vincent Tavernier <vince.tavernier@gmail.com>
2016-12-17 22:58:49 +0100

    Simpler snake_case method

--- lib/docker/api/utils.rb
+++ lib/docker/api/utils.rb
@@ -2,10 +2,7 @@ module Docker
   module Api
     module Utils
       def self.snake_case(str)
-        str.gsub(/::/, '/').
-          gsub('IDs', 'Ids').
-          gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-          gsub(/([a-z\d])([A-Z])/,'\1_\2').
+        str.gsub(/([a-z])([A-Z])/, '\1_\2').
           tr("-", "_").
           downcase
       end
