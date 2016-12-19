4c4a91f03fef7e42c8e3a1dfa18d4d837a5df2f5
Vincent Tavernier <vince.tavernier@gmail.com>
2016-12-20 00:42:36 +0100

    Node instance methods

--- lib/docker/api/node.rb
+++ lib/docker/api/node.rb
@@ -3,6 +3,17 @@ require "docker/api/entity"
 module Docker
   module Api
     class Node < Entity
+      def inspect_node
+        client.node_inspect(id, self)
+      end
+
+      def remove(params = {})
+        client.node_remove(id, params)
+      end
+
+      def update(config, params = {})
+        client.node_update(config, params)
+      end
     end
   end
 end
