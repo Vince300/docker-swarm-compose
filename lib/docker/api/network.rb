af39020f6fcdb17cb119de99f2729c77f97c8e4d
Vincent Tavernier <vince.tavernier@gmail.com>
2016-12-20 00:41:39 +0100

    Network instance methods

--- lib/docker/api/network.rb
+++ lib/docker/api/network.rb
@@ -3,6 +3,21 @@ require "docker/api/entity"
 module Docker
   module Api
     class Network < Entity
+      def inspect_network
+        client.network_inspect(id, self)
+      end
+
+      def connect(config)
+        client.network_connect(id, config)
+      end
+
+      def disconnect(config)
+        client.network_disconnect(id, config)
+      end
+
+      def remove
+        client.network_remove(id)
+      end
     end
   end
 end
