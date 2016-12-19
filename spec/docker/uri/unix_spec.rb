d2383c50b9c3029ea14b28fdd7c9b697e2cd1970
Vincent Tavernier <vince.tavernier@gmail.com>
2016-12-19 21:56:46 +0100

    Fix existing unit tests

--- spec/docker/uri/unix_spec.rb
+++ spec/docker/uri/unix_spec.rb
@@ -4,7 +4,7 @@ describe URI::UNIX do
   it "parses unix:///my/test.sock:/hello?hello=true" do
     url = URI('unix:///my/test.sock:/hello?hello=true')
 
-    expect(url.hostname).to eq('/my/test.sock')
+    expect(url.socket_path).to eq('/my/test.sock')
     expect(url.path).to eq('/hello')
     expect(url.request_uri).to eq('/hello?hello=true')
   end
